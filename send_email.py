b2e4c0105a43e1e9c85c4b124784af6d7b691fdd
RiccardoDAndrea <riccardo.dandrea@live.de>
2023-08-02 16:46:33 +0200

    bug fix

--- send_email.py
+++ send_email.py
@@ -1,6 +1,7 @@
 import smtplib, ssl
 import os
-
+import pandas as pd
+import yfinance as yf
 
 port = 465
 smtp_server = "smtp.gmail.com"
@@ -15,3 +16,33 @@ context = ssl.create_default_context()
 with smtplib.SMTP_SSL(smtp_server, port, context=context) as server:
     server.login(USERNAME,PASSWORD)
     server.sendmail(USERNAME,USERNAME,message)
+
+#### E N D _ S E N D I N G _ E - M A I L
+
+
+
+msft = yf.Ticker("MSFT")
+print(msft)
+"""
+returns
+<yfinance.Ticker object at 0x1a1715e898>
+"""
+
+# get stock info
+msft.info
+
+"""
+returns:
+{
+ 'quoteType': 'EQUITY',
+ 'quoteSourceName': 'Nasdaq Real Time Price',
+ 'currency': 'USD',
+ 'shortName': 'Microsoft Corporation',
+ 'exchangeTimezoneName': 'America/New_York',
+  ...
+ 'symbol': 'MSFT'
+}
+"""
+
+# get historical market data, here max is 5 years.
+msft.history(period="max")
