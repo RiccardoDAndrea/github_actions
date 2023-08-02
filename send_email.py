8485eaa5a31be92758af18e690f5a3830605500d
RiccardoDAndrea <riccardo.dandrea@live.de>
2023-08-02 23:04:21 +0200

    new packages

--- send_email.py
+++ send_email.py
@@ -8,36 +8,73 @@ from email.mime.multipart import MIMEMultipart
 from matplotlib import pyplot as plt
 from io import BytesIO
 
-#### G E T _ S T O C K _ P R I C E S ####
+# #### G E T _ S T O C K _ P R I C E S ####
 
+# tickers = ['AAPL', 'BYDDF', 'EONGY', 'LNVGF', 'NIO', 'PLUN.F', 'TSLA', 'TKA.DE', 'XIACF']
+# end_date = datetime.today()
+# start_date = end_date - timedelta(days=2 * 365)
+
+# close_df = pd.DataFrame()
+
+# for ticker in tickers:
+#     data = yf.download(ticker, start=start_date, end=end_date)
+#     close_df[ticker] = data['Close']
+
+# close_df.reset_index(inplace=True)
+
+# #### P L O T ####
+
+# plt.figure(figsize=(10, 6))
+# for ticker in tickers:
+#     plt.plot(close_df['Date'], close_df[ticker], label=ticker)
+# plt.legend()
+# plt.xlabel('Date')
+# plt.ylabel('Close Price')
+# plt.title('Close Prices of Selected Stocks')
+
+# # Speichern des Graphen als Bild
+# image_file_name = 'stock_prices.png'
+# plt.savefig(image_file_name)
+# plt.close()
+
+#### S E N D I N G _ E - M A I L ####
+import pandas as pd
+from matplotlib import pyplot as plt
+
+# Import of data
+df = pd.read_csv('close_data.csv')
+print(df)
 tickers = ['AAPL', 'BYDDF', 'EONGY', 'LNVGF', 'NIO', 'PLUN.F', 'TSLA', 'TKA.DE', 'XIACF']
-end_date = datetime.today()
-start_date = end_date - timedelta(days=2 * 365)
+stock_buy_price = [110.96, 24.95, 11.47, 1.419, 47.81, 45.544, 159.95, 6.799474, 1.6198]
 
-close_df = pd.DataFrame()
+# Calculate percentage change
+stock_close_price = df.iloc[-1, 1:].tolist()
+percentage_change = [(close_price - buy_price) / buy_price * 100 for close_price, buy_price in zip(stock_close_price, stock_buy_price)]
 
-for ticker in tickers:
-    data = yf.download(ticker, start=start_date, end=end_date)
-    close_df[ticker] = data['Close']
+# Visualization of Stocks
+fig, ax = plt.subplots()
 
-close_df.reset_index(inplace=True)
+bar_labels = [f"{ticker} ({change:.2f}%)" for ticker, change in zip(tickers, percentage_change)]
+bar_colors = ['tab:red', 'tab:blue', 'tab:green', 'tab:orange', 'tab:gray', 'tab:cyan', 'tab:purple', 'tab:pink', 'tab:brown']
 
-#### P L O T ####
+ax.bar(tickers, percentage_change, label=bar_labels, color=bar_colors)
 
-plt.figure(figsize=(10, 6))
-for ticker in tickers:
-    plt.plot(close_df['Date'], close_df[ticker], label=ticker)
-plt.legend()
-plt.xlabel('Date')
-plt.ylabel('Close Price')
-plt.title('Close Prices of Selected Stocks')
+ax.set_ylabel('Percentage Change')
+ax.set_title('Percentage Change in Stock Prices')
+ax.legend(title='Stocks and Percentage Change')
+
+plt.show()
 
 # Speichern des Graphen als Bild
 image_file_name = 'stock_prices.png'
 plt.savefig(image_file_name)
 plt.close()
 
-#### S E N D I N G _ E - M A I L ####
+
+
+
+
+
 
 port = 465
 smtp_server = "smtp.gmail.com"
