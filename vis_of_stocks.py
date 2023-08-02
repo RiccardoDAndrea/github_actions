2151d0584dd2789e558f763444738db818e2d7ef
RiccardoDAndrea <riccardo.dandrea@live.de>
2023-08-02 22:42:28 +0200

    fs

--- vis_of_stocks.py
+++ vis_of_stocks.py
@@ -5,6 +5,8 @@ from matplotlib import pyplot as plt
 # Import of data
 df = pd.read_csv('close_data.csv')
 print(df)
+tickers = ['AAPL', 'BYDDF', 'EONGY', 'LNVGF', 'NIO', 'PLUN.F', 'TSLA', 'TKA.DE', 'XIACF']
+stock_buy_price = [110.96, 24.95, 11.47, 47.81]
 # Viszualisation of Stocks
 
 
