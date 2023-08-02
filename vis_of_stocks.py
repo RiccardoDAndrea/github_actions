974a28ed25f32ea9edf24f0a0711715898143304
RiccardoDAndrea <riccardo.dandrea@live.de>
2023-08-02 23:01:50 +0200

    neue branch

--- vis_of_stocks.py
+++ vis_of_stocks.py
@@ -1,4 +1,3 @@
-# Packages
 import pandas as pd
 from matplotlib import pyplot as plt
 
@@ -6,9 +5,22 @@ from matplotlib import pyplot as plt
 df = pd.read_csv('close_data.csv')
 print(df)
 tickers = ['AAPL', 'BYDDF', 'EONGY', 'LNVGF', 'NIO', 'PLUN.F', 'TSLA', 'TKA.DE', 'XIACF']
-stock_buy_price = [110.96, 24.95, 11.47, 47.81]
-# Viszualisation of Stocks
+stock_buy_price = [110.96, 24.95, 11.47, 1.419, 47.81, 45.544, 159.95, 6.799474, 1.6198]
 
+# Calculate percentage change
+stock_close_price = df.iloc[-1, 1:].tolist()
+percentage_change = [(close_price - buy_price) / buy_price * 100 for close_price, buy_price in zip(stock_close_price, stock_buy_price)]
 
+# Visualization of Stocks
+fig, ax = plt.subplots()
 
+bar_labels = [f"{ticker} ({change:.2f}%)" for ticker, change in zip(tickers, percentage_change)]
+bar_colors = ['tab:red', 'tab:blue', 'tab:green', 'tab:orange', 'tab:gray', 'tab:cyan', 'tab:purple', 'tab:pink', 'tab:brown']
 
+ax.bar(tickers, percentage_change, label=bar_labels, color=bar_colors)
+
+ax.set_ylabel('Percentage Change')
+ax.set_title('Percentage Change in Stock Prices')
+ax.legend(title='Stocks and Percentage Change')
+
+plt.show()
