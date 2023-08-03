7ab1fe66ffae2d19ec754627273bd9c1a55943d8
RiccardoDAndrea <riccardo.dandrea@live.de>
2023-08-03 22:09:09 +0200

    change in figsize

--- test.py
+++ test.py
@@ -13,25 +13,27 @@ start_date = end_date - timedelta(days= 2 * 365)
 
 close_df = pd.DataFrame()
 
-for ticker in tickers:
-    data = yf.download(ticker, start = start_date, end = end_date)
-    close_df[ticker] = data['Close']
+# for ticker in tickers:
+#     data = yf.download(ticker, start = start_date, end = end_date)
+#     close_df[ticker] = data['Close']
 
-close_df.reset_index(inplace=True)
-
+# close_df.reset_index(inplace=True)
+df = pd.read_csv('close_data.csv')
 #### P L O T ####
 
 tickers = ['AAPL', 'BYDDF', 'EONGY', 'LNVGF', 'NIO', 'PLUN.F', 'TSLA', 'TKA.DE', 'XIACF']
 stock_buy_price = [110.96, 24.95, 11.47, 1.419, 47.81, 45.544, 159.95, 6.799474, 1.6198]
 
+# close_df muss bei df in zeile 28
 # Calculate percentage change
-stock_close_price = close_df.iloc[-1, 1:].tolist()
+
+stock_close_price = df.iloc[-1, 1:].tolist()
 percentage_change = [(close_price - buy_price) / buy_price * 100 for close_price, buy_price in zip(stock_close_price, stock_buy_price)]
 percentage_change = [round(change, 2) for change in percentage_change]
 
 
 # Visualization of Stocks
-plt.figure(figsize = (10,6))
+plt.figure(figsize = (15,7))
 bars = plt.bar(tickers, percentage_change, color = 'tab:blue')
 
 
@@ -47,4 +49,4 @@ plt.title('Percentage Change in Stock Prices')
 # Speichern des Graphen als Bild
 image_file_name = 'stock_prices.png'
 plt.savefig(image_file_name)
-plt.close()
+plt.show()
