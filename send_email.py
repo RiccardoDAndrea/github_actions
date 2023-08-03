91512615c1055f4635438aa6ff6c777301abbbe9
RiccardoDAndrea <riccardo.dandrea@live.de>
2023-08-03 22:09:09 +0200

    test adding graph

--- send_email.py
+++ send_email.py
@@ -1,43 +1,3 @@
-import smtplib, ssl
-import os
-import pandas as pd
-import yfinance as yf
-from datetime import datetime, timedelta
-from email.mime.text import MIMEText
-from email.mime.multipart import MIMEMultipart
-from matplotlib import pyplot as plt
-from io import BytesIO
-
-# #### G E T _ S T O C K _ P R I C E S ####
-
-# tickers = ['AAPL', 'BYDDF', 'EONGY', 'LNVGF', 'NIO', 'PLUN.F', 'TSLA', 'TKA.DE', 'XIACF']
-# end_date = datetime.today()
-# start_date = end_date - timedelta(days=2 * 365)
-
-# close_df = pd.DataFrame()
-
-# for ticker in tickers:
-#     data = yf.download(ticker, start=start_date, end=end_date)
-#     close_df[ticker] = data['Close']
-
-# close_df.reset_index(inplace=True)
-
-# #### P L O T ####
-
-# plt.figure(figsize=(10, 6))
-# for ticker in tickers:
-#     plt.plot(close_df['Date'], close_df[ticker], label=ticker)
-# plt.legend()
-# plt.xlabel('Date')
-# plt.ylabel('Close Price')
-# plt.title('Close Prices of Selected Stocks')
-
-# # Speichern des Graphen als Bild
-# image_file_name = 'stock_prices.png'
-# plt.savefig(image_file_name)
-# plt.close()
-
-#### S E N D I N G _ E - M A I L ####
 import smtplib
 import ssl
 import os
@@ -63,18 +23,19 @@ for ticker in tickers:
 
 close_df.reset_index(inplace=True)
 
-#### P L O T ####
+#### P E R S O N A L _ S T O C K S ####
 
 tickers = ['AAPL', 'BYDDF', 'EONGY', 'LNVGF', 'NIO', 'PLUN.F', 'TSLA', 'TKA.DE', 'XIACF']
 stock_buy_price = [110.96, 24.95, 11.47, 1.419, 47.81, 45.544, 159.95, 6.799474, 1.6198]
 
-# Calculate percentage change
+#### C A L C I L A T E _ P E R C E N T A G E _ C H A N G E
+
 stock_close_price = close_df.iloc[-1, 1:].tolist()
 percentage_change = [(close_price - buy_price) / buy_price * 100 for close_price, buy_price in zip(stock_close_price, stock_buy_price)]
 percentage_change = [round(change, 2) for change in percentage_change]
 
 
-# Visualization of Stocks
+#### V I S U A L I Z A T I O N _ O F _ S T O C K S
 plt.figure(figsize = (10,6))
 bars = plt.bar(tickers, percentage_change, color = 'tab:blue')
 
@@ -88,7 +49,7 @@ plt.ylabel('Percentage Change')
 plt.title('Percentage Change in Stock Prices')
 #ax.legend(title='Stocks and Percentage Change')
 
-# Speichern des Graphen als Bild
+# S A V E _ A S _ I M A G E
 image_file_name = 'stock_prices.png'
 plt.savefig(image_file_name)
 plt.close()
@@ -100,7 +61,8 @@ smtp_server = "smtp.gmail.com"
 USERNAME = os.environ.get('USER_EMAIL')
 PASSWORD = os.environ.get('USER_PASSWORD')
 
-# Erstelle die E-Mail
+# C R E A T E _ M A I L 
+
 message = MIMEMultipart()
 message['From'] = USERNAME
 message['To'] = USERNAME
@@ -108,7 +70,8 @@ message['Subject'] = "GitHub Email Report"
 body = "Das ist eine automatisierte Mail mit einem Graphen der Schlusskurse der ausgewählten Aktien im Anhang."
 message.attach(MIMEText(body, 'plain'))
 
-# Füge den Graphen als Anhang hinzu
+# A T T C H M E N T 
+
 with open(image_file_name, 'rb') as attachment:
     part = MIMEText(attachment.read(), 'png', _charset='utf-8')
     part.add_header('Content-Disposition', 'attachment', filename=image_file_name)
--- test.py
+++ test.py
@@ -13,24 +13,34 @@ start_date = end_date - timedelta(days= 2 * 365)
 
 close_df = pd.DataFrame()
 
-for ticker in tickers:
-    data = yf.download(ticker, start = start_date, end = end_date)
-    close_df[ticker] = data['Close']
+# for ticker in tickers:
+#     data = yf.download(ticker, start = start_date, end = end_date)
+#     close_df[ticker] = data['Close']
 
-close_df.reset_index(inplace=True)
-
+# close_df.reset_index(inplace=True)
+df = pd.read_csv('close_data.csv')
 #### P L O T ####
+#### G E T _ S T O C K _ P R I C E S ####
+
+tickers = ['AAPL', 'BYDDF', 'EONGY', 'LNVGF', 'NIO', 'PLUN.F', 'TSLA', 'TKA.DE', 'XIACF']
+end_date = datetime.today()
+start_date = end_date - timedelta(days= 2 * 365)
+
+
+
+#### P E R S O N A L _ S T O C K S ####
 
 tickers = ['AAPL', 'BYDDF', 'EONGY', 'LNVGF', 'NIO', 'PLUN.F', 'TSLA', 'TKA.DE', 'XIACF']
 stock_buy_price = [110.96, 24.95, 11.47, 1.419, 47.81, 45.544, 159.95, 6.799474, 1.6198]
 
-# Calculate percentage change
-stock_close_price = close_df.iloc[-1, 1:].tolist()
+#### C A L C I L A T E _ P E R C E N T A G E _ C H A N G E
+
+stock_close_price = df.iloc[-1, 1:].tolist()
 percentage_change = [(close_price - buy_price) / buy_price * 100 for close_price, buy_price in zip(stock_close_price, stock_buy_price)]
 percentage_change = [round(change, 2) for change in percentage_change]
 
 
-# Visualization of Stocks
+#### V I S U A L I Z A T I O N _ O F _ S T O C K S
 plt.figure(figsize = (10,6))
 bars = plt.bar(tickers, percentage_change, color = 'tab:blue')
 
@@ -44,7 +54,7 @@ plt.ylabel('Percentage Change')
 plt.title('Percentage Change in Stock Prices')
 #ax.legend(title='Stocks and Percentage Change')
 
-# Speichern des Graphen als Bild
+# S A V E _ A S _ I M A G E
 image_file_name = 'stock_prices.png'
 plt.savefig(image_file_name)
-plt.close()
+plt.show()
