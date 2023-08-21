6ee1e9706acfd95bb561aa6ce570b85f6a81de43
RiccardoDAndrea <riccardo.dandrea@live.de>
2023-08-21 21:03:00 +0200

    anpassungen der Graphen

--- testing_vis.py
+++ testing_vis.py
@@ -6,6 +6,8 @@ from datetime import datetime, timedelta
 import pandas as pd
 from matplotlib import pyplot as plt
 from matplotlib.dates import date2num, DateFormatter
+from pandas.plotting import table
+
 
 end_date = datetime.today()
 start_date = end_date - timedelta(days= 2 * 365)
@@ -51,7 +53,9 @@ sum_of_stocks = [price * quantity for price, quantity in zip(stock_close_price, 
     
 #### V I S U A L I Z A T I O N _ O F _ S T O C K S
 
-fig, ((ax1, ax2), (ax3, ax4)) = plt.subplots(2, 2, figsize=(15, 8))
+fig, ((ax1, ax2), (ax3, ax4)) = plt.subplots(2, 2, figsize=(12, 8))
+plt.subplots_adjust(wspace=0.4, hspace=0.4)  # Horizontale und vertikale Abstände anpassen
+
 bars = ax2.barh(tickers, percentage_change, color = 'tab:blue')
 
 
@@ -60,38 +64,70 @@ bar_colors = ['tab:red', 'tab:blue', 'tab:green', 'tab:orange', 'tab:gray', 'tab
 
 
 # F I R S T _ C H A R T -> Säulen Diagram
-print('Your share portfolio consists of the following shares:' + str(tickers))
-print('You bought them at the following prices: :'+ str(stock_buy_price))
-print(df)
+last_week_data = df.tail(7)
+
+# Daten der letzten Woche vorbereiten
+last_week_data['Date'] = pd.to_datetime(last_week_data['Date'])  # Konvertieren in Datetime-Objekte
+last_week_dates = last_week_data['Date'].dt.strftime('%Y-%m-%d')  # Datum formatieren
+percentage_changes = (last_week_data.iloc[:, 1:-1] / last_week_data.iloc[:, 1:-1].shift(1) - 1) * 100
 
 
+# Prozentuale Veränderung der Aktienkurse über die letzte Woche plotten (Liniendiagramm)
+ax1.plot(last_week_dates, percentage_changes, marker='o')
 
+# Prozentuale Veränderung der Aktienkurse über die letzte Woche plotten (Liniendiagramm)
+ax1.plot(last_week_dates, percentage_changes, marker='o')
+ax1.set_xticks(last_week_dates)  # X-Ticks setzen
 
+# Datumsbeschriftungen um 45 Grad drehen
+ax1.set_xticklabels(last_week_dates, rotation=45)
 
-# Zweite Achse (Balkendiagramm)
+ax1.set_xlabel('Date')
+ax1.set_ylabel('Percentage Change')
+ax1.set_title('Stock Price Performance Last Week')
+
+# S E C O N D _ P L O T -> Linen Diagramm
 bars = ax2.barh(tickers, percentage_change, color=bar_colors)
 ax2.bar_label(bars, labels=bar_labels, label_type='center')
 ax2.set_xlabel('Percentage Change')
-ax2.set_xticklabels([])  # X-Achsenbeschriftungen entfernen
+ # X-Achsenbeschriftungen entfernen
+ax2.set_xticklabels([], rotation=45)
 ax2.set_title('Percentage Change in Stock Prices')
-# S E C O N D _ P L O T -> Linen Diagramm
+
+
+# T H I R D _ P L O T -> Linen Diagramm
+
 
 for ticker in tickers:
     ax3.plot(df['Month_Year'], df[ticker], label=ticker)
 
 ax3.set_xlabel('Month_Year')
+
 ax3.set_ylabel('Stock Price')
+
 ax3.set_title('Stock Prices Over Time')
 ax3.legend(title='Stocks and Percentage Change')
 
 
+# F O U R T H _ P L O T -> Linen Diagramm
+
+for ticker in tickers:
+    ax4.plot(df['Month_Year'], df[ticker], label=ticker)
+ax4.set_xticklabels(df['Month_Year'].dt.strftime('%Y-%m') , rotation=45)
+ax4.set_xlabel('Month_Year')
+ax4.set_ylabel('Stock Price')
+ax4.set_title('Stock Prices Over Time')
+ax4.legend(title='Stocks and Percentage Change')
+
 
 #### T H R I D _ P L O T _ C R E A T I O N _ O F _ P I E _ C H A R T -> Kuchen Diagramm 
 
-plt.figure(figsize=(15,8))
+plt.figure(figsize=(5,8))
 plt.title('Share risk distribution')
 plt.pie(sum_of_stocks, labels=tickers)
 plt.tight_layout()
+plt.legend(loc='upper left', bbox_to_anchor=(1, 1))  # Legende außerhalb des Diagramms platzieren
+
 pie_image_file_name = 'share_risk_distribution.png'
 plt.savefig(pie_image_file_name)
 
