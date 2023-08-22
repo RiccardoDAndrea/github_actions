9b33859334f3dd32403a301959c36021b2211c94
RiccardoDAndrea <riccardo.dandrea@live.de>
2023-08-22 18:20:20 +0200

    korregierungen der zeilen durch geführt

--- testing_vis.py
+++ testing_vis.py
@@ -45,34 +45,30 @@ percentage_change = [(close_price - buy_price) / buy_price * 100 for close_price
 percentage_change = [round(change, 2) for change in percentage_change]              # runden auf 2 nach komma stellen
 
 sum_of_stocks = [price * quantity for price, quantity in zip(stock_close_price, quantitiy_of_stocks)]
-    
-#### V I S U A L I Z A T I O N _ O F _ S T O C K S
-
-fig, ((ax1, ax2), (ax3, ax4)) = plt.subplots(2, 2, figsize=(20, 12))
-plt.subplots_adjust(wspace=0.3, hspace=0.6)  # Horizontale und vertikale Abstände anpassen
-
-bars = ax2.barh(tickers, percentage_change, color = 'tab:blue')
-
-
-bar_labels = [f"{ticker} ({change:.2f}%)" for ticker, change in zip(tickers, percentage_change)]
-bar_colors = ['tab:red', 'tab:blue', 'tab:green', 'tab:orange', 'tab:gray', 'tab:cyan', 'tab:purple', 'tab:pink', 'tab:brown']
-
-# F I R S T _ C H A R T -> Säulen Diagram
 last_week_data = df.tail(7)
 
 # Daten der letzten Woche vorbereiten
 last_week_data['Date'] = pd.to_datetime(last_week_data['Date'])  # Konvertieren in Datetime-Objekte
 last_week_dates = last_week_data['Date'].dt.strftime('%Y-%m-%d')  # Datum formatieren
-percentage_changes = (last_week_data.iloc[:, 1:-1] / last_week_data.iloc[:, 1:-1].shift(1) - 1) * 100
+percentage_changes = (last_week_data.iloc[:, 1:-1] / last_week_data.iloc[:, 1:-1].shift(1) - 1) * 100    
 
-# Prozentuale Veränderung der Aktienkurse über die letzte Woche plotten (Liniendiagramm)
+bar_labels = [f"{ticker} ({change:.2f}%)" for ticker, change in zip(tickers, percentage_change)]
+bar_colors = ['tab:red', 'tab:blue', 'tab:green', 'tab:orange', 'tab:gray', 'tab:cyan', 'tab:purple', 'tab:pink', 'tab:brown']
+
+#### V I S U A L I Z A T I O N _ O F _ S T O C K S
+
+fig, ((ax1, ax2), (ax3, ax4)) = plt.subplots(2, 2, figsize=(18, 9))
+plt.subplots_adjust(wspace=0.3, hspace=0.6)  # Horizontale und vertikale Abstände anpassen
+
+# F I R S T _ C H A R T -> Säulen Diagram
+
 ax1.plot(last_week_dates, percentage_changes, marker='o')
 ax1.set_xticks(last_week_dates)  # X-Ticks setzen
 ax1.set_xticklabels(last_week_dates, rotation=45)
 ax1.set_xlabel('Date')
 ax1.set_ylabel('Percentage Change')
 ax1.set_title('Stock Price Performance Last Week')
-
+ax1.legend(loc='best', mode='expand',ncol=3, bbox_to_anchor=(0,1,1,2))
 # S E C O N D _ P L O T -> Linen Diagramm
 
 bars = ax2.barh(tickers, percentage_change, color=bar_colors)
@@ -95,7 +91,8 @@ ax4.set_xticklabels(df['Month_Year'].dt.strftime('%Y-%m') , rotation=45)
 ax4.set_xlabel('Month_Year')
 ax4.set_ylabel('Stock Price')
 ax4.set_title('Stock Prices Over the last two years')
-# ax4.legend(title='Stocks and Percentage Change')
+plt.legend(loc='best', mode='expand',ncol=3, bbox_to_anchor=(0,1,1,2))
+
 
 
 #### S E C O N D _ P L O T _ C R E A T I O N _ O F _ P I E _ C H A R T -> Kuchen Diagramm 
@@ -104,7 +101,6 @@ plt.figure(figsize=(5,8))
 plt.title('Share risk distribution')
 plt.pie(sum_of_stocks, labels=tickers)
 plt.tight_layout()
-# plt.legend(loc='upper left', bbox_to_anchor=(1, 1))  # Legende außerhalb des Diagramms platzieren
 pie_image_file_name = 'share_risk_distribution.png'
 plt.savefig(pie_image_file_name)
 
